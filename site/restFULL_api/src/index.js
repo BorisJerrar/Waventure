21309e97329775bc0aded76d3522710502b8dd49
charles decodin <charles.decodin@outlook.fr>
2020-06-03 10:38:01 +0200

    init routes

--- site/restFULL_api/src/index.js
+++ site/restFULL_api/src/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
-
+const port = process.env.port|| 4000
 const app = express()
+
 
 app.use(express.json())
 app.use(cors())
@@ -15,7 +15,24 @@ app.use(
   )
 
 
-const port = process.env.port|| 4000
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+/* app.get('/actors', getActors)
+app.get('/categories', getCategories)
+app.get('/episodes', getEpisodes)
+app.get('/favorites', getFavorites)
+app.get('/listen', getListen)
+app.get('/role', getRole)
+app.get('/saisons', getSaisons)
+app.get('/series', getSeries)
+app.get('/series_actors', getSeriesActors)
+app.get('/series_categories', getSeriesCategories)
+app.get('/synopsis', getSynopsis)
+app.get('/users', getUsers) */
+
+
 
 
 
