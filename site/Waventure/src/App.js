40411110fecfceb56e42451aaef8b4c8ef9b92f3
Boris Jerrar <borisjerrar@MacBook-Pro.fritz.box>
2020-06-03 10:48:23 +0200

    Init React Project

---
+++ site/Waventure/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+<img className='App-logo' src='./img/wavelogodark.svg' alt='waventure Logo' />
+    </div>
+  );
+}
+
+export default App;
