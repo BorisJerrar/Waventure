f10baf123c9c201f8c6455ab46cbccc192078eb1
charles decodin <charles.decodin@outlook.fr>
2020-06-04 12:03:48 +0200

    bug fetch

--- site/Waventure/src/components/Categorie.jsx
+++ site/Waventure/src/components/Categorie.jsx
@@ -1,20 +1,64 @@
 import React, { useState, useEffect } from 'react'
 
 export default function Categorie() {
-    const [series, setSeries] = useState([])
+    const [image, setImage] = useState([])
+    const [imageUrlSeries, setImageUrlSeries]= useState([])
+
     const fetchSeries = async() =>{
-        const reponse = await fetch(`http://localhost:4000/test?url=survivaure`)
-        const data = await reponse.blob()
-        console.log(data);
-        
-        setSeries(URL.createObjectURL(data))
-
-        
+        const reponse = await fetch(`http://localhost:4000/series`)
+        const data = await reponse.json()     
+        setImageUrlSeries([...imageUrlSeries, data.map((each)=>{return(each.image)})])
+        console.log(data); 
     }
-    useEffect(()=>{
-        fetchSeries()
-    }, [])
+    console.log(imageUrlSeries);
 
+
+     const fetchImages = async() =>{
+         for(let i = 0; i < imageUrlSeries.length; i++){
+             setImage(...image, 'http://localhost:4000/test?url=')
+         }
+            try{
+                const reponse = Promise.all(image.map((item, index)=>{
+                    fetch(item + imageUrlSeries[index]).then(resp => resp.json())
+                
+                })).then(json=>{
+                    console.log(json);
+                })
+            } catch (e){
+                console.log(e);
+                
+            }
+      /*         console.log(imageUrlSeries[i]);
+        const reponse = await fetch(`http://localhost:4000/test?url=${imageUrlSeries[i]}`)
+        const data = await reponse.blob()
+        setImage([...image, URL.createObjectURL(data)])
+        console.log(data); */
+         
+       
+
+
+} 
+
+
+/* videoUrls= async () => {
+    let i=0;
+    let urllist=[]
+    for(i;i< this.state.data.length;i++){
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.state.data[i].name}&key=xxxxxxxxxxx0`)
+        const json = await response.json()
+        urllist.push(json.items[0])
+        console.log({urllist})
+      }
+   } */
+
+useEffect(()=>{
+    fetchSeries()
+   
+}, [])
+
+useEffect(()=>{
+    fetchImages()
+}, [imageUrlSeries])
 
  
      return(
@@ -28,11 +72,9 @@ export default function Categorie() {
             )
         })}
     </div> */
-<>
-    <div>yoyoyoyyo</div>
-    <img src={series} alt=""/>
-    </>
+    <div>
+    <img src={image} style={{'width': '300px'}} alt=""/>
+    </div>
 )    
 
- }   
-
+    }
