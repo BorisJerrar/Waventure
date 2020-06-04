0b76602ba546f0dbdb924d3125a7209de19c48b5
valentin-ce <valentin.cellier@gmail.com>
2020-06-04 09:36:44 +0200

    add queries by id

--- site/api/src/queries/series_categories.js
+++ site/api/src/queries/series_categories.js
@@ -10,6 +10,17 @@ const getSeriesCategories = (request, response) => {
   })
 }
 
+const getSeriesCategoriesById = (request, response) => {
+  const series_categories_id = parseInt(request.params.series_categories_id)
+  db.query('SELECT * FROM series_categories WHERE series_id = $1', [series_categories_id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
 module.exports = {
-    getSeriesCategories
+    getSeriesCategories,
+    getSeriesCategoriesById
 }
