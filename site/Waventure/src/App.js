22816c8ef110921a5f0db0834a59dfb9721f45f9
Boris Jerrar <contact@borisjerrar.fr>
2020-06-04 14:09:07 +0200

    adding props for header

--- site/Waventure/src/App.js
+++ site/Waventure/src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React ,{useState} from "react";
 import './App.css';
 import Categorie from './components/Categorie.jsx'
 import  Header from './components/Header'
 
 
 function App() {
+  const [trigger, setTrigger] = useState(false)
+  const triggering = () => {
+    if(trigger){
+    setTrigger(!trigger)
+    }
+  }
   return (
-    <div className='App'>
+    <div className='App' onClick={triggering}>
 <>
-    <Header/>
+    <Header
+    trigger={trigger}
+    setTrigger={setTrigger}
+    />
     <Categorie/>
 </>
     </div>
