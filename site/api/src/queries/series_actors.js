8ae82dd08ce4ed795ef2eb7c208f6e364ac2ecba
valentin-ce <valentin.cellier@gmail.com>
2020-06-04 11:39:04 +0200

    add method post delete update for users, series, series_actors

--- site/api/src/queries/series_actors.js
+++ site/api/src/queries/series_actors.js
@@ -20,7 +20,51 @@ const getSeriesActorsById = (request, response) => {
   })
 }
 
+const createSeriesActors = (request, response) => {
+    const serieid = request.query.serieid
+    const actorid = request.query.actorid
+
+    db.query('INSERT INTO series_actors ( serieid, actorid ) VALUES ($1, $2)', [ serieid, actorid ], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Series_actors added with ID: ${results.insertId}`)
+    })
+}
+
+const updateSeriesActors = (request, response) => {
+    const series_actors_id = parseInt(request.params.series_actors_id)
+    const serieid = request.query.serieid
+    const actorid = request.query.actorid
+
+    db.query(
+        'UPDATE series_actors SET serieid = $1, actorid = $2 WHERE series_actors_id = $3', [serieid, actorid, series_actors_id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`Series_actors modified with ID: ${series_actors_id}`)
+        }
+    )
+}
+
+const deleteSeriesActors = (request, response) => {
+    const series_actors_id = parseInt(request.params.series_actors_id)
+
+    db.query('DELETE FROM series_actors WHERE series_actors_id = $1', [series_actors_id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`Series_actors deleted with ID: ${series_actors_id}`)
+    })
+}
+
+
+
 module.exports = {
     getSeriesActors,
-    getSeriesActorsById
+    getSeriesActorsById,
+    createSeriesActors,
+    updateSeriesActors,
+    deleteSeriesActors
 }
