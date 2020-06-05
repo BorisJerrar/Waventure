4c2b6a85e52564945051ff7c88ff9e9670466401
Boris Jerrar <contact@borisjerrar.fr>
2020-06-05 14:23:49 +0200

    editing db

--- site/Waventure/src/components/Player.js
+++ site/Waventure/src/components/Player.js
@@ -4,8 +4,8 @@ import "../style/Player.css";
 
 export default function Player() {
     const serverPath = process.env.REACT_APP_SERVER_PATH
-
     const [episodeNumber, setEpisodeNumber] = useState(1)
+    const [cover, setCover] = useState('')
     const [episodeInfos, setEpisodeInfos] = useState({})
     const [urlAudio, setUrlAudio] = useState(``)
     useEffect(() => {
@@ -13,11 +13,22 @@ export default function Player() {
             const track = await fetch(`${serverPath}/episodesNumber/${episodeNumber}`)
             const data = await track.json()
             await setEpisodeInfos(...data)
-            console.log(...data);
-            
             await setUrlAudio(`${serverPath}/sound/${data[0].mp3file}`)
         }
+        const fetchingImgAudio = async() => {
+            const reponse = await fetch(`http://localhost:4000/series/2`)
+            const data = await reponse.json()
+            await setCover(data[0].image)
+        }
+        const fetchingInfo = async() => {
+            const reponse = await fetch(`http://localhost:4000/sagaInfo/3`)
+            const data = await reponse.json()
+            console.log(data);
+            
+        }
         fetchingAudio()
+        fetchingImgAudio()
+        fetchingInfo()
     },[episodeNumber,serverPath])
     const nextSaga = () => {
         console.log(episodeInfos);
@@ -29,23 +40,26 @@ export default function Player() {
     }
     const header = () => {
 return(
-    <>
+    <div className='playerHeader'>
     <p>Episode 1 | La Legende</p>
     <p>Nico {'&'} Matt</p>
-    </>
+    </div>
 )
     }
     const footer = () => {
 return(
-    <>
+    <div className='playerFooter'>
     <p>La Legende de Xantah</p>
     <p>Synopsys</p>
     <p>En savoir plus</p>
-    </>
+    </div>
 )
     }
     return (
+        <div className='playerWarper'>
+            <img src={`${serverPath}/images/${cover}`} alt='mettre desd alt' />
         <AudioPlayer
+        layout={'horizontal'}
         header={header()}
         footer={footer()}
         src={urlAudio}
@@ -56,5 +70,6 @@ return(
         onClickNext={() => {nextSaga()}}
         onEnded={() => {nextSaga()}}
       />
+      </div>
     )
 }
