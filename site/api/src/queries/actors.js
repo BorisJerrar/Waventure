38dbd78ddeeb7baa17a4bd8dac62463b0638ca94
valentin-ce <valentin.cellier@gmail.com>
2020-06-05 09:40:58 +0200

    adding mains methods for all api entity

--- site/api/src/queries/actors.js
+++ site/api/src/queries/actors.js
@@ -20,7 +20,49 @@ const getActorById = (request, response) => {
   })
 }
 
+const createActors = (request, response) => {
+    const name = request.query.name
+
+    db.query('INSERT INTO Actors ( name ) VALUES ($1)', [ name  ], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`Actors added with ID: ${results.actors_Id}`)
+    })
+}
+
+const updateActors = (request, response) => {
+    const actors_id = parseInt(request.params.actors_id)
+    const name = request.query.name
+    db.query(
+        'UPDATE Actors SET name = $1 WHERE actors_id = $2', [name, actors_id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`actors modified with ID: ${actors_id}`)
+        }
+    )
+}
+
+const deleteActors = (request, response) => {
+    const actors_id = parseInt(request.params.actors_id)
+
+    db.query('DELETE FROM actors WHERE actors_id = $1', [actors_id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`actors deleted with ID: ${actors_id}`)
+    })
+}
+
+
+
+
 module.exports = {
     getActors,
-    getActorById
+    getActorById,
+    createActors,
+    updateActors,
+    deleteActors
 }
--- site/api/src/queries/role.js
+++ site/api/src/queries/role.js
@@ -20,8 +20,50 @@ const getRoleById = (request, response) => {
   })
 }
 
+const createRole = (request, response) => {
+    const actorid = request.query.actorid
+    const character = request.query.character
+
+    db.query('INSERT INTO role ( actorid, character ) VALUES ($1, $2)', [ actorid, character ], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`role added with ID: ${results.insertId}`)
+    })
+}
+
+const updateRole = (request, response) => {
+    const roles_id = parseInt(request.params.roles_id)
+    const actorid = request.query.actorid
+    const character = request.query.character
+
+    db.query(
+        'UPDATE role SET actorid = $1, character = $2 WHERE roles_id = $3', [actorid, character, roles_id],
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).send(`Role modified with ID: ${roles_id}`)
+        }
+    )
+}
+
+const deleteRole = (request, response) => {
+    const roles_id = parseInt(request.params.roles_id)
+
+    db.query('DELETE FROM role WHERE roles_id = $1', [roles_id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).send(`role deleted with ID: ${roles_id}`)
+    })
+}
+
 
 module.exports = {
     getRole,
-    getRoleById
+    getRoleById,
+    createRole,
+    updateRole,
+    deleteRole
 }
