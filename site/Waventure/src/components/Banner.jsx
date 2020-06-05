8a65eacae866e4daa33ed0a5501c47d44d9a5332
charles decodin <charles.decodin@outlook.fr>
2020-06-05 14:58:43 +0200

    btn play integrate

--- site/Waventure/src/components/Banner.jsx
+++ site/Waventure/src/components/Banner.jsx
@@ -22,9 +22,17 @@ export default function Banner() {
 
     return (
         <div className="wedetteContainer">
-            <img className="wedetteCover" src={`${url}/clydevanilla.jpg`} alt="" />
+            <div className="wedetteCover">
+              <img className="wedetteCoverImage" src={`${url}/clydevanilla.jpg`} alt="" />  
+              <img src="./img/btnPlay.svg" className="btnPlay" alt=""/>  
+            </div>
+            <div >
+            <span><img src="./img/logoWedette.svg" alt="W"/></span>
             <h2 className="wedetteIndicator">Wedette</h2>
+            </div>
+            
             <div className="wedetteDescription">
+                
                 <h3 className="wedetteTitle">CLYDE VANILLA</h3>
                 <p className="wedetteSynopsis">Le jeune garçon Clyde part en quête de la pierre du Destin à travers l’univers.
                 Il rencontrera notamment le sergent Moustachios, fondateur de la guilde des chasseurs de trésors,
@@ -40,3 +48,4 @@ export default function Banner() {
         </div>
     )
 }
+
