27b0866ea1a7878a0444fb3bc939c51f2cba8fa5
charles decodin <charles.decodin@outlook.fr>
2020-06-07 08:03:02 +0200

    compatible to new db

--- site/api/src/queries/actors.js
+++ site/api/src/queries/actors.js
@@ -2,7 +2,7 @@ const db = require('../../db/database')
 
 
 const getActors = (request, response) => {
-  db.query('SELECT * FROM actors ORDER BY actors_id ASC', (error, results) => {
+  db.query('SELECT * FROM actor ORDER BY actor_id ASC', (error, results) => {
     if (error) {
       throw error
     }
@@ -11,8 +11,8 @@ const getActors = (request, response) => {
 }
 
 const getActorById = (request, response) => {
-  const actors_id = parseInt(request.params.actors_id)
-  db.query('SELECT * FROM actors WHERE actors_id = $1', [actors_id], (error, results) => {
+  const actor_id = parseInt(request.params.actors_id)
+  db.query('SELECT * FROM actor WHERE actor_id = $1', [actor_id], (error, results) => {
     if (error) {
       throw error
     }
@@ -23,36 +23,36 @@ const getActorById = (request, response) => {
 const createActors = (request, response) => {
     const name = request.query.name
 
-    db.query('INSERT INTO Actors ( name ) VALUES ($1)', [ name  ], (error, results) => {
+    db.query('INSERT INTO actor ( name ) VALUES ($1)', [ name ], (error, results) => {
         if (error) {
             throw error
         }
-        response.status(201).send(`Actors added with ID: ${results.actors_Id}`)
+        response.status(201).send(`Actors added with ID: ${results.actor_Id}`)
     })
 }
 
 const updateActors = (request, response) => {
-    const actors_id = parseInt(request.params.actors_id)
+    const actor_id = parseInt(request.params.actor_id)
     const name = request.query.name
     db.query(
-        'UPDATE Actors SET name = $1 WHERE actors_id = $2', [name, actors_id],
+        'UPDATE Actors SET name = $1 WHERE actor_id = $2', [name, actor_id],
         (error, results) => {
             if (error) {
                 throw error
             }
-            response.status(200).send(`actors modified with ID: ${actors_id}`)
+            response.status(200).send(`actors modified with ID: ${actor_id}`)
         }
     )
 }
 
 const deleteActors = (request, response) => {
-    const actors_id = parseInt(request.params.actors_id)
+    const actor_id = parseInt(request.params.actor_id)
 
-    db.query('DELETE FROM actors WHERE actors_id = $1', [actors_id], (error, results) => {
+    db.query('DELETE FROM actor WHERE actor_id = $1', [actor_id], (error, results) => {
         if (error) {
             throw error
         }
-        response.status(200).send(`actors deleted with ID: ${actors_id}`)
+        response.status(200).send(`actors deleted with ID: ${actor_id}`)
     })
 }
 
