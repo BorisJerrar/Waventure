7abb5f233038dca6c22f2f89e62dd403d57e5f1f
valentin-ce <valentin.cellier@gmail.com>
2020-06-07 12:49:34 +0200

    fix error in account

--- site/api/src/queries/account.js
+++ site/api/src/queries/account.js
@@ -2,7 +2,7 @@ const db = require('../../db/database')
 
 
 const getAccount = (request, response) => {
-  db.query('SELECT * FROM user ORDER BY account_id ASC', (error, results) => {
+  db.query('SELECT * FROM account ORDER BY account_id ASC', (error, results) => {
     if (error) {
       throw error
     }
@@ -12,7 +12,7 @@ const getAccount = (request, response) => {
 
 const getAccountById = (request, response) => {
   const account_id = parseInt(request.params.account_id)
-  db.query('SELECT * FROM user WHERE account_id = $1', [account_id], (error, results) => {
+  db.query('SELECT * FROM account WHERE account_id = $1', [account_id], (error, results) => {
     if (error) {
       throw error
     }
@@ -21,27 +21,27 @@ const getAccountById = (request, response) => {
 }
 
 const createAccount = (request, response) => {
-    const { username, first_name, last_name, email, birth_date, password } = request.body
+    const { accountname, first_name, last_name, email, birth_date, password } = request.body
 
-    db.query('INSERT INTO user ( username, first_name, last_name, email, birth_date, password ) VALUES ($1, $2, $3, $4, $5, crypt($6, gen_salt(\'md5\')))', [username, first_name, last_name, email, birth_date, password], (error, results) => {
+    db.query('INSERT INTO account ( username, first_name, last_name, email, birth_date, password ) VALUES ($1, $2, $3, $4, $5, crypt($6, gen_salt(\'md5\')))', [username, first_name, last_name, email, birth_date, password], (error, results) => {
         if (error) {
             throw error
         }
-        response.status(201).send(`User added with ID: ${results.insertId}`)
+        response.status(201).send(`account added with ID: ${results.insertId}`)
     })
 }
 
 const updateAccount = (request, response) => {
     const account_id = parseInt(request.params.account_id)
-    const { username, first_name, last_name, email, birth_date, password } = request.body
+    const { accountname, first_name, last_name, email, birth_date, password } = request.body
 
     db.query(
-        'UPDATE user SET username = $1, first_name = $2, last_name = $3, email = $4, birth_date = $5, password = $6 WHERE account_id = $7', [username, first_name, last_name, email, birth_date, password, account_id],
+        'UPDATE account SET username = $1, first_name = $2, last_name = $3, email = $4, birth_date = $5, password = $6 WHERE account_id = $7', [username, first_name, last_name, email, birth_date, password, account_id],
         (error, results) => {
             if (error) {
                 throw error
             }
-            response.status(200).send(`User modified with ID: ${account_id}`)
+            response.status(200).send(`account modified with ID: ${account_id}`)
         }
     )
 }
@@ -49,11 +49,11 @@ const updateAccount = (request, response) => {
 const deleteAccount = (request, response) => {
     const account_id = parseInt(request.params.account_id)
 
-    db.query('DELETE FROM user WHERE account_id = $1', [account_id], (error, results) => {
+    db.query('DELETE FROM account WHERE account_id = $1', [account_id], (error, results) => {
         if (error) {
             throw error
         }
-        response.status(200).send(`User deleted with ID: ${account_id}`)
+        response.status(200).send(`account deleted with ID: ${account_id}`)
     })
 }
 
