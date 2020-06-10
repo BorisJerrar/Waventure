e736223e548f66fcbcf4c3dce19618e2bbafa319
charles decodin <charles.decodin@outlook.fr>
2020-06-10 12:07:07 +0200

    create route serie by categorie

--- site/api/db/database.js
+++ site/api/db/database.js
@@ -1,6 +1,5 @@
 const Pool = require('pg').Pool
 const connect = new Pool({
-<<<<<<< HEAD
   user: 'postgres',
   host: 'localhost',
   database: 'waventure',
--- site/api/src/queries/serie_serie_category_category.js
+++ site/api/src/queries/serie_serie_category_category.js
@@ -1,5 +1,17 @@
 const db = require('../../db/database')
 
 const getSerieByCategory = (request, response) =>{
-    db.query('SELECT * FROM catgeory INNER JOIN serie_category ON serie_category.category_id = category.category_id INNER JOIN serie ON serie.serie_id = serie_category.serie_id WHERE category.category_id')
+    const category_id = parseInt(request.params.category_id)
+    console.log(category_id);
+    
+    db.query('SELECT * FROM category INNER JOIN serie_category ON serie_category.category_id = category.category_id INNER JOIN serie ON serie.serie_id = serie_category.serie_id WHERE category.category_id = $1', [category_id], (error, results)=>{
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+module.exports = {
+    getSerieByCategory
 }
