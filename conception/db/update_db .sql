088e266b18b9ef752b33e7578215c62a7d1f5b96
Boris Jerrar <contact@borisjerrar.fr>
2020-06-11 11:19:34 +0200

    alter table

--- conception/db/update_db .sql
+++ conception/db/update_db .sql
@@ -1,4 +1,2 @@
-//execute into psql to update your db 
-
-DELETE FROM serie_category WHERE serie_category_id = 90;
-DELETE FROM serie_category WHERE serie_category_id = 35;
+ALTER TABLE episode
+RENAME COLUMN duration TO episode_duration;
