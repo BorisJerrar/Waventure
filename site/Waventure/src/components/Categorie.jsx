cf8633e7e9f3f550706583d1fbd68c664902a25c
Boris Jerrar <contact@borisjerrar.fr>
2020-06-15 15:33:12 +0200

    improving catalogue

--- site/Waventure/src/components/Categorie.jsx
+++ site/Waventure/src/components/Categorie.jsx
@@ -1,33 +1,46 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "../style/Categorie.css";
 import { Slide } from "react-slideshow-image";
 
 export default function Categorie({ category, lunchingEpisode }) {
   const [series, setSeries] = useState([]);
+  const [hover, setHover] = useState(false);
+  const [information, setInformation] = useState(false);
+  const [hoverItem, setHoverItem] = useState([]);
+  const [synopsis, setSynopsis] = useState('');
+  const [dataHover, setDataHover] = useState({});
   const [lengthSeries, setLengthSeries] = useState("");
   const url = process.env.REACT_APP_DYNAMIC_IMG_PATH;
-  const pathImg = process.env.REACT_APP_STATIC_IMG_PATH;
+  const urlimg = process.env.REACT_APP_STATIC_IMG_PATH;
+  const server = process.env.REACT_APP_SERVER_PATH;
 
   const lunchingEpisodeCategorie = (item) => {
     lunchingEpisode(item.serie_id);
   };
-
-  const fetchSeries = async () => {
-    const response = await fetch(
-      `http://localhost:4000/serieCategory/${category}`
-    );
-    const data = await response.json();
-    let temp = [];
-    for (let i = 0; i < Math.ceil(data.length / 5); i++) {
-      temp.push(data.slice(i * 5, i * 5 + 5));
-    }
-    setSeries(temp);
-    setLengthSeries(data.length);
-  };
-
   useEffect(() => {
+    const fetchSeries = async () => {
+      const response = await fetch(`${server}/serieCategory/${category}`);
+      const data = await response.json();
+      let temp = [];
+      for (let i = 0; i < Math.ceil(data.length / 5); i++) {
+        temp.push(data.slice(i * 5, i * 5 + 5));
+      }
+      setSeries(temp);
+      setLengthSeries(data.length);
+    };
     fetchSeries();
-  }, [category]);
+    const fetchSerieInformation = async () => {
+      const response = await fetch(
+        `http://localhost:4000/sagaInfo/${hoverItem.serie_id}`
+      );
+      const data = await response.json();
+      setDataHover(data[0]);
+      setSynopsis(data[0].body);
+    };
+    if (hover) {
+      fetchSerieInformation();
+    }
+  }, [category, hover, hoverItem.serie_id, server]);
 
   let properties = {
     indicators: true,
@@ -42,45 +55,97 @@ export default function Categorie({ category, lunchingEpisode }) {
       indicators: false,
     };
   }
-
-  const zoomItem = (e) => {
-    console.log(e.target.style.width);
-    e.target.style.width = "17vw";
-    e.target.style.margin = "0vw";
-    e.target.style.boxShadow = '0px 3px 6px rgba(0, 0, 0, 0.356)';
+  const settingHover = (item) => {
+    setHover(true);
+    setHoverItem(item);
+    console.log(dataHover);
+    
   };
-
-  const reinitializeSize = (e) => {
-    e.target.style.width = "15vw";
-    e.target.style.margin = "1vw";
-    e.target.style.boxShadow = '0px 3px 4px rgba(0, 0, 0, 0.356)';
+  const unsettingHover = () => {
+    setHover(false);
+    setInformation(false);
   };
-
+  const informationShow = (e) => {
+    e.stopPropagation()
+    setInformation(!information);
+  };
   return (
     <div className="catalog">
       <h2 className="catalogTitle">{category}</h2>
-
       <div className="containerSlide">
         <Slide {...properties}>
           {series.map((array, arrIndex) => {
             return (
-              <div
-              className='categoryContainer'
-                key={arrIndex}
-                style={{ }}
-              >
+              <div className="categoryContainer" key={arrIndex} style={{}}>
                 {array.map((item, index) => {
                   return (
-                    <img
+                    <div
                       key={index}
-                      className="imageSlideShow"
-                      onMouseOver={zoomItem}
-                      onMouseOut={reinitializeSize}
-                      onClick={()=> {lunchingEpisodeCategorie(item)}}
-                      style={{ width: `15vw`, margin: "1vw",transition : "ease-in-out all 0.2s"}}
-                      src={`${url}/${item.image}`}
-                      alt={"cover of" + item.title}
-                    />
+                      className={"hoverInformationContainer"}
+                      onMouseEnter={() => settingHover(item)}
+                      onMouseLeave={unsettingHover}
+                    >
+                      <div
+                        className="hoverInformation"
+                        onClick={() => {
+                          lunchingEpisodeCategorie(item);
+                        }}
+                      >
+                                                                      <h6
+                          style={
+                            information
+                              ? { visibility: "visible" }
+                              : { visibility: "hidden" }
+                          }
+                        >
+                          {synopsis}
+                        </h6>
+                        <img
+                          className="InformationCategory"
+                          onClick={informationShow}
+                          src={`${urlimg}/information.svg`}
+                          alt="play button"
+                        />
+                        <p
+                          style={
+                            information
+                              ? { visibility: "hidden" }
+                              : { visibility: "visible" }
+                          }
+                        >
+                          {item.title}
+                        </p>
+                        <p
+                          style={
+                            information
+                              ? { visibility: "hidden" }
+                              : { visibility: "visible" }
+                          }
+                        >
+                          {item.author}
+                        </p>
+                        <p
+                          style={
+                            information
+                              ? { visibility: "hidden" }
+                              : { visibility: "visible" }
+                          }
+                        >
+                          Ep.1 S.2
+                        </p>
+                        <i className="watchTime"></i>
+                        <img
+                          className="playLogoCategory"
+                          src={`${urlimg}/btnPlay.svg`}
+                          alt="play button"
+                        />
+                      </div>
+                      <img
+                        className="imageSlideShow"
+                        src={`${url}/${item.image}`}
+                        alt={"cover of" + item.title}
+                      />
+                    </div>
                   );
                 })}
               </div>
