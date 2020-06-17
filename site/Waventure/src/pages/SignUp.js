8da7503fcc349ad5cb516ac371283d8dba4bc2ed
valentin-ce <valentin.cellier@gmail.com>
2020-06-17 10:14:41 +0200

    add style for log form and home

--- site/Waventure/src/pages/SignUp.js
+++ site/Waventure/src/pages/SignUp.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import { Link, Redirect } from 'react-router-dom';
-import "../style/SignUp.css"
+import "../style/LoginForm.css"
 
 function SignUp(props) {
     const pathLogo = process.env.REACT_APP_STATIC_IMG_PATH
@@ -69,7 +69,8 @@ function SignUp(props) {
     }
 
     return (
-        <main>
+        <div className="bg-container">
+        <main className="user-form">
             <div className="waventureLogo">
                 <img src={`${pathLogo}/waventureLogo.svg`} alt="Waventure Logo" />
                 <h1>WAVENTURE</h1>
@@ -131,6 +132,7 @@ function SignUp(props) {
                 </div>
             </form>
         </main>
+        </div>
     )
 }
 
