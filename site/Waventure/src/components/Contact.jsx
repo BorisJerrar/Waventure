152a45dc476abda60288554572a8d1fdc7e67a5c
charles decodin <charles.decodin@outlook.fr>
2020-06-26 10:01:52 +0200

    write file with new serie

--- site/Waventure/src/components/Contact.jsx
+++ site/Waventure/src/components/Contact.jsx
@@ -3,28 +3,40 @@ import "../style/Contact.css";
 import MainProfilInfo from './MainInfoProfil'
 
 export default function Contact({user}) {
-    const axios = require('axios')
-    const [fileAudio, setFileAudio] = useState(null)
-   
-  
-    const handleSoundUpload = e =>{
-        setFileAudio(e.target.files[0])
+    const server = process.env.REACT_APP_SERVER_PATH;
+    const [newSerie, setNewSerie] = useState({
+        account: user[0].account_id,
+        email: user[0].email,
+        title: "",
+        body:""
+    })
+
+    const updateTitle = e =>{
+        setNewSerie({
+            ...newSerie,
+            title: e.target.value
+        })
+    }
+    const updateBody = e =>{
+        setNewSerie({
+            ...newSerie,
+            body: e.target.value
+        })
     }
 
-    const handleToServer = () => {    
-        const data = new FormData()
-        data.append('file', fileAudio)
-        console.log(data);
-        axios.post('http://localhost:4000/upload', data, {
-
-        }).then(res=>{
-            console.log(res.statusText);
-            
+    const sendSerie = () =>{
+        fetch(`${server}/sendSerie`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newSerie)
         })
-}
+    }
 
+console.log(newSerie);
 
-
+    
 
     return (
         
@@ -54,16 +66,16 @@ export default function Contact({user}) {
 
         <div className="formNewSaga">
             <p className="favoriteSagaTitle">Je souhaite que Waventure diffuse ma Saga</p>
-            <input  type="text" placeholder="Titre de la saga"/>
-            <textarea  placeholder="Résumé de la série"></textarea>
-            <input type="file" onChange={handleSoundUpload}/>
-            <button onClick={handleToServer}>Envoyer</button>
+            <input  type="text" placeholder="Titre de la saga" onChange={updateTitle}/>
+            <textarea  placeholder="Résumé de la série" onChange={updateBody}></textarea>
+            {/* <input type="file" onChange={handleSoundUpload}/> */}
+            <button onClick={sendSerie}>Envoyer</button>
             <p className="favoriteSagaParagraph ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras amet nulla leo imperdiet eu in eu hendrerit 
                 duis. In iaculis integer feugiat ac metus. Dolor netus leo, sollicitudin nisi. 
                 Auctor urna in est massa nunc iaculis.</p>
         </div>
 
-        <footer>
+        <footer className="footerContact">
             <p>Ma saga favorite n'est pas ici</p>
             <p>Proposer ma propre saga</p>
             <p>Contacter Waventure</p>
