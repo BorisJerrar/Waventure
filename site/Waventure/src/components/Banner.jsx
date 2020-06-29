2f4bb9313f65309e16d7dd52825631c9ffa96c9f
Boris Jerrar <contact@borisjerrar.fr>
2020-06-29 11:35:02 +0200

    reusing utils in banner

--- site/Waventure/src/components/Banner.jsx
+++ site/Waventure/src/components/Banner.jsx
@@ -1,117 +1,104 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import "../style/Banner.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import axios from 'axios'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import fetchingNewEpisode from "../utiles/fetchingNewEpisode";
+import fetchingExsistingEpisode from "../utiles/fetchingExsistingEpisode";
+import didHeAlreadyBegin from "../utiles/didHeAlreadyBegin";
 
 export default function Banner({ lunchingEpisode }) {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    // data one serie star
-    const [wedette, setWedette] = useState([]);
+  // data one serie star
+  const [wedette, setWedette] = useState([]);
 
-    //variable environnement 
-    const server = process.env.REACT_APP_SERVER_PATH
-    const url = process.env.REACT_APP_DYNAMIC_IMG_PATH
-    
-    useEffect(() => {
-      /**
-       * @param serverPath path server stack in .env
-       * @param setState  hook useState 
-       * @param serverParams
-       */
-      const fetchWedette = async () => {
-          const response = await fetch(`${server}/serieSynopsis/1`)
-          const data = await response.json()
-          setWedette(data[0])
-      }
-        fetchWedette()
-    }, [server])
-    const lunchingEpisodeCategorie = (item) => {
-        const didHeAlreadyBegin = () => {
-          var config = {
-            method: "get",
-            url: `${server}/listenVerificator?serie_id=${item.serie_id}`,
-            headers: {
-              "x-access-token": token,
-            },
-          };
-          axios(config)
-            .then(function (response) {
-              if (response.data.length === 0) {
-                fetchingEpisode();
-              } else {
-                fetchingExsistingEpisode(...response.data);
-                
-              }
-            })
-            .catch(function (error) {
-              console.log(error);
+  //variable environnement
+  const server = process.env.REACT_APP_SERVER_PATH;
+  const url = process.env.REACT_APP_DYNAMIC_IMG_PATH;
+
+  useEffect(() => {
+    /**
+     * @param serverPath path server stack in .env
+     * @param setState  hook useState
+     * @param serverParams
+     */
+    const fetchWedette = async () => {
+      const response = await fetch(`${server}/serieSynopsis/1`);
+      const data = await response.json();
+      setWedette(data[0]);
+    };
+    fetchWedette();
+  }, [server]);
+  /* First of all, cheking if user has already begun the audiodrama on click*/
+  const lunchingEpisodeCategorie = (item) => {
+    didHeAlreadyBegin(item, function (config) {
+      axios(config)
+        .then(function (response) {
+          /* If not, lunching first episode of the audiodrama cliked*/
+          if (response.data.length === 0) {
+            fetchingNewEpisode(item, function (fetchconfig) {
+              axios(fetchconfig)
+                .then(function (response) {
+                  return lunchingEpisode(item.serie_id, 0);
+                })
+                .catch(function (error) {
+                  console.log(error);
+                });
             });
-        };
-        const fetchingEpisode = async () => {
-          const fetching = await fetch(`${server}/sagaInfo/${item.serie_id}`);
-          const response = await fetching.json();
-          const dataInfo = await response;
-          AddingNew(dataInfo);
-        };
-        const fetchingExsistingEpisode = async (info) => {
-          const fetching = await fetch(`${server}/sagaInfo/${item.serie_id}`);
-          const response = await fetching.json();
-    
-          resume(info, response);
-        };
-        const AddingNew = (sagas) => {
-          var creatingNew = {
-            method: "post",
-            url: `${server}/listen?serie_id=${sagas[0].serie_id}&episode_id=${sagas[0].episode_id}`,
-            headers: {
-              "x-access-token": token,
-            },
-          };
-          axios(creatingNew)
-            .then(function (response) {
-              lunchingEpisode(item.serie_id, 0);
-            })
-            .catch(function (error) {
-              console.log(error);
+            /*  If yes, fetching datas, and resuming episode*/
+          } else {
+            fetchingExsistingEpisode(...response.data, function (
+              item,
+              response
+            ) {
+              lunchingEpisode(
+                item.serie_id,
+                response.findIndex(
+                  (UniqueItem) => UniqueItem.episode_id === item.episode_id
+                )
+              );
             });
-        };
-        const resume = (userInfo, dataInfo) => {
-          lunchingEpisode(
-            item.serie_id,
-            dataInfo.findIndex((item) => item.episode_id === userInfo.episode_id)
-          );
-        };
-    
-        didHeAlreadyBegin();
-      };
-    
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    });
+  };
 
-    return (
-        <div className={"wedetteContainer"}>
-            <div className="wedetteCover" onClick={() => lunchingEpisodeCategorie(wedette)}>
-                <img className="wedetteCoverImage" src={`${url}/${wedette && wedette.image_lg ? wedette.image_lg : ''}`} alt="" />
-                <FontAwesomeIcon
-                    className="btnPlay"
-                    icon={['fa', 'play-circle']}
-                    size="4x"
-                />
-            </div>
-            <div className="wedetteLogoContainer">
-                <span><img src="./img/logoWedette.svg" alt="W" /></span>
-                <h2 className="wedetteIndicator">edette</h2>
-            </div>
+  return (
+    <div className={"wedetteContainer"}>
+      <div
+        className="wedetteCover"
+        onClick={() => lunchingEpisodeCategorie(wedette)}
+      >
+        <img
+          className="wedetteCoverImage"
+          src={`${url}/${wedette && wedette.image_lg ? wedette.image_lg : ""}`}
+          alt=""
+        />
+        <FontAwesomeIcon
+          className="btnPlay"
+          icon={["fa", "play-circle"]}
+          size="4x"
+        />
+      </div>
+      <div className="wedetteLogoContainer">
+        <span>
+          <img src="./img/logoWedette.svg" alt="W" />
+        </span>
+        <h2 className="wedetteIndicator">edette</h2>
+      </div>
 
-            <div className="wedetteDescription">
-
-                <h3 className="wedetteTitle">{wedette.title}</h3>
-                <p className="wedetteSynopsis">
-                    {wedette.body}
-                </p>
-            </div>
-            <img className="backgroundImg" src={`${url}/${wedette && wedette.image_bg ? wedette.image_bg : ''}`} alt={`Bg_image ${wedette && wedette.title ? wedette.title : ''}`} />
-        </div>
-    )
+      <div className="wedetteDescription">
+        <h3 className="wedetteTitle">{wedette.title}</h3>
+        <p className="wedetteSynopsis">{wedette.body}</p>
+      </div>
+      <img
+        className="backgroundImg"
+        src={`${url}/${wedette && wedette.image_bg ? wedette.image_bg : ""}`}
+        alt={`Bg_image ${wedette && wedette.title ? wedette.title : ""}`}
+      />
+    </div>
+  );
 }
-
