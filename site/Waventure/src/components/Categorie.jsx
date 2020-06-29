f642355c9a58c13884bbdfad903e3be55af37234
Boris Jerrar <contact@borisjerrar.fr>
2020-06-29 10:25:17 +0200

    adding comments

--- site/Waventure/src/components/Categorie.jsx
+++ site/Waventure/src/components/Categorie.jsx
@@ -3,15 +3,14 @@ import CategoryUnique from "./CategoryUnique";
 import "../style/Categorie.css";
 import { Slide } from "react-slideshow-image";
 import axios from "axios";
-import fetchingNewEpisode from './fetchingNewEpisode'
-import fetchingExsistingEpisode from './fetchingExsistingEpisode'
-import fetchSeries from './fetchSeries'
-import didHeAlreadyBegin from './didHeAlreadyBegin'
-import Context from '../context/context'
+import fetchingNewEpisode from "./fetchingNewEpisode";
+import fetchingExsistingEpisode from "./fetchingExsistingEpisode";
+import fetchSeries from "./fetchSeries";
+import didHeAlreadyBegin from "./didHeAlreadyBegin";
+import Context from "../context/context";
 
 export default function Categorie({ category, lunchingEpisode }) {
-
-  const { matches, setMaches} = useContext(Context)
+  const { matches, setMaches } = useContext(Context);
   const token = localStorage.getItem("token");
   const [series, setSeries] = useState([]);
   const [hover, setHover] = useState(false);
@@ -21,68 +20,85 @@ export default function Categorie({ category, lunchingEpisode }) {
   const url = process.env.REACT_APP_DYNAMIC_IMG_PATH;
   const server = process.env.REACT_APP_SERVER_PATH;
 
-
+  /* First of all, cheking if user has already begun the audiodrama on click*/
   const lunchingEpisodeCategorie = (item) => {
-    didHeAlreadyBegin(item, function(config) {
+    didHeAlreadyBegin(item, function (config) {
       axios(config)
         .then(function (response) {
+          /* If not, lunching first episode of the audiodrama cliked*/
           if (response.data.length === 0) {
-            fetchingNewEpisode(item, function(fetchconfig){
+            fetchingNewEpisode(item, function (fetchconfig) {
               axios(fetchconfig)
-               .then(function (response) {
-                 return lunchingEpisode(item.serie_id, 0);
-               })
-               .catch(function (error) {
-                 console.log(error);
-               }); 
-           })
+                .then(function (response) {
+                  return lunchingEpisode(item.serie_id, 0);
+                })
+                .catch(function (error) {
+                  console.log(error);
+                });
+            });
+                      /*  If yes, fetching datas, and resuming episode*/
           } else {
-            fetchingExsistingEpisode(...response.data, function(item, response){
+            fetchingExsistingEpisode(...response.data, function (
+              item,
+              response
+            ) {
               lunchingEpisode(
                 item.serie_id,
-                response.findIndex((UniqueItem) => UniqueItem.episode_id === item.episode_id)
+                response.findIndex(
+                  (UniqueItem) => UniqueItem.episode_id === item.episode_id
+                )
               );
-            });  
+            });
           }
         })
         .catch(function (error) {
           console.log(error);
-        })
-    })
+        });
+    });
   };
-useEffect(() => {
-  let mounted = true
-  window.addEventListener("resize", () => {setTimeout(() => {
-      if (mounted){setMaches(window.innerWidth)}
-    }, 300)
+  /* Listening to windows resize to display audiodrama in fuction of width */
+  useEffect(() => {
+    let mounted = true;
+    window.addEventListener("resize", () => {
+      setTimeout(() => {
+        if (mounted) {
+          setMaches(window.innerWidth);
+        }
+      }, 300);
+    });
   });
-})
-useEffect(() => {
-  fetchSeries(category, matches, function(temp){
-    setSeries(temp)})
+  useEffect(() => {
+        /* Fetching  audiodrama to displaying it*/
+    fetchSeries(category, matches, function (temp) {
+      setSeries(temp);
+    });
+    /* Fetching  audio informations on hover*/
     const fetchSerieInformation = async () => {
       const response = await fetch(`${server}/sagaInfo/${hoverItem.serie_id}`);
       const data = await response.json();
-       setSynopsis(data[0].body);
+      setSynopsis(data[0].body);
     };
     if (hover) {
       fetchSerieInformation();
     }
   }, [category, hover, hoverItem.serie_id, server, token, matches]);
-
+/*  properties for the npm <Slide />*/
   let properties = {
     indicators: true,
     autoplay: false,
     transitionDuration: 500,
   };
+  /* On Mouse Enter => Trigger Hover Hooks and changing HoverItems informations*/
   const settingHover = (item) => {
     setHover(true);
     setHoverItem(item);
   };
-  const unsettingHover = () => {
-    setHover(false);
-    setInformation(false);
-  };
+    /* On Mouse Leave => Trigger Hover Hooks and reinitialize display of synopsis*/
+    const unsettingHover = () => {
+      setHover(false);
+      setInformation(false);
+    };
+    /* On Click => display of synopsis*/
   const informationShow = (e) => {
     e.stopPropagation();
     setHover(!hover);
